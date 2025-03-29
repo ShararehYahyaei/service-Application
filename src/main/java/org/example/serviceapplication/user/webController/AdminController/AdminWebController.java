0932a33dbf276be005dcd6b25c494ddbb956441a
sharareh <Sharareh.yahyaei@gmail.com>
2025-03-29 18:53:53 +0100

    create add admin controller and get all customers and specialists by admin user and add an image for home page

--- src/main/java/org/example/serviceapplication/user/webController/AdminController/AdminWebController.java
+++ src/main/java/org/example/serviceapplication/user/webController/AdminController/AdminWebController.java
@@ -1,8 +1,12 @@
 package org.example.serviceapplication.user.webController.AdminController;
 
+import org.example.serviceapplication.Category.dto.ServiceCategoryResponse;
+import org.example.serviceapplication.Category.service.ServiceCategoryInterface;
 import org.example.serviceapplication.user.dto.CustomerResponseDto;
 import org.example.serviceapplication.user.dto.SpecialistResponseDto;
 import org.example.serviceapplication.user.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,10 +17,13 @@ import java.util.List;
 public class AdminWebController {
 
     private final UserService userService;
+    private final ServiceCategoryInterface categoryService;
 
 
-    public AdminWebController(UserService userService) {
+    public AdminWebController(UserService userService,
+                              ServiceCategoryInterface categoryService) {
         this.userService = userService;
+        this.categoryService = categoryService;
     }
 
 
@@ -42,6 +49,13 @@ public class AdminWebController {
         List<SpecialistResponseDto> allSpecialists = userService.getAllSpecialists();
         model.addAttribute("specialists", allSpecialists);
         return "specialist-list";
+    }
+
+
+    @GetMapping("/getAllCategories")
+    public ResponseEntity<List<ServiceCategoryResponse>> getAllCategories() {
+        List<ServiceCategoryResponse> allCategories = categoryService.getAllCategories();
+        return new ResponseEntity<>(allCategories, HttpStatus.OK);
     }
 
 }
