46c2f0e2190d62706cb75d00db86a970dff7c83e
sharareh <Sharareh.yahyaei@gmail.com>
2025-03-30 23:59:55 +0200

    fix back to main page button

---
+++ src/main/java/org/example/serviceapplication/user/webController/specialistController/SpecialistWebController.java
@@ -0,0 +1,32 @@
+package org.example.serviceapplication.user.webController.specialistController;
+
+import org.example.serviceapplication.subCategory.dto.SubServiceCategories;
+import org.example.serviceapplication.subCategory.service.SubServiceCategoryInterface;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class SpecialistWebController {
+    private final SubServiceCategoryInterface subservice;
+
+    public SpecialistWebController(SubServiceCategoryInterface subservice) {
+        this.subservice = subservice;
+    }
+
+
+    @GetMapping("/sub-services")
+    public String showSubServices(Model model) {
+        List<SubServiceCategories> allSubServices = subservice.getAllSubServiceCatgories();
+        model.addAttribute("allSubServices", allSubServices);
+        return "sub-services";
+    }
+
+    @PostMapping("/add-subservice")
+    public String addSubServiceRedirect() {
+        return "redirect:/sub-services";
+    }
+}
