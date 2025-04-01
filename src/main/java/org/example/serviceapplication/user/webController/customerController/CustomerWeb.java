69a3068c24c71632e9f23421f86c6b0bfa2201d7
sharareh <Sharareh.yahyaei@gmail.com>
2025-04-01 13:13:24 +0200

    add new page for adding customer number and customer request ID

--- src/main/java/org/example/serviceapplication/user/webController/customerController/CustomerWeb.java
+++ src/main/java/org/example/serviceapplication/user/webController/customerController/CustomerWeb.java
@@ -1,6 +1,7 @@
 package org.example.serviceapplication.user.webController.customerController;
 
 
+import org.example.serviceapplication.offer.dto.OfferDto;
 import org.example.serviceapplication.request.dto.CustomerRequestDto;
 import org.example.serviceapplication.subCategory.dto.SubServiceCategories;
 import org.example.serviceapplication.subCategory.service.SubServiceCategoryInterface;
@@ -8,12 +9,16 @@ import org.example.serviceapplication.user.enumPackage.Role;
 import org.example.serviceapplication.user.exception.UserHasWrongRole;
 import org.example.serviceapplication.user.model.User;
 import org.example.serviceapplication.user.service.customerService.CustomerService;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -67,5 +72,11 @@ public class CustomerWeb {
         customerService.createRequest(customer, customerRequest);
         return "redirect:/customerRequests";
     }
+
+    @GetMapping("/all-my-suggestions")
+    public String showSuggestions() {
+        return "all-my-suggestions";
+    }
+
 
 }
